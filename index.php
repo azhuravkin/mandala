34aa3fe14026441f9abb819ce1d3bb42265b9fa8
root <root@adm-zhuravkin.webpay.by>
2014-01-15 17:28:57 +0300

    add var x3,y3,x4,y4

--- index.php
+++ index.php
@@ -7,11 +7,16 @@
     $black = imagecolorallocate($image, 0, 0, 0);
     $red = imagecolorallocate($image, 255, 0, 0);
 
+    $x3 = $size / 4;
+    $x4 = $size / 2;
+
+    $y3 = $size / 4;
+    $y4 = $size / 2;
+
     imagesetthickness($image, 3);
 
-//    imageellipse($image, $size / 2, $size / 2, $size, $size, $black);
-    imagearc($image, $size / 2, $size / 2, $size, $size, 0, 180, $black);
-    imagearc($image, $size / 2, $size / 2, $size, $size, 180, 360, $black);
+    imagearc($image, $x4, $y4, $size, $size, 0, 180, $black);
+    imagearc($image, $x4, $y4, $size, $size, 180, 360, $black);
 
     $side_of_square = $size / sqrt(2);
     $square_xy_1 = ($size - $side_of_square) / 2;
@@ -20,34 +25,34 @@
 
     $polygon = array(
 	$side_of_square / 2 + $square_xy_1, $square_xy_1,
-	$size - $size / 4, $size / 4,
+	$size - $size / 4, $y3,
 	$side_of_square + $square_xy_1, $side_of_square / 2 + $square_xy_1,
 	$size - $size / 4, $size - $size / 4,
 	$side_of_square / 2 + $square_xy_1, $side_of_square + $square_xy_1,
 	$size / 4, $size - $size / 4,
 	$square_xy_1, $side_of_square / 2 + $square_xy_1,
-	$size / 4, $size / 4
+	$x3, $y3
     );
     imagepolygon($image, $polygon, 8, $black);
 
-    imageline($image, $size / 2, $square_xy_1 / 2, $size / 2, $square_xy_1, $black);
+    imageline($image, $x4, $square_xy_1 / 2, $size / 2, $square_xy_1, $black);
     imageline($image, $square_xy_2, $size / 2, $square_xy_2 + $square_xy_1 / 2, $size / 2, $black);
-    imageline($image, $size / 2, $square_xy_2, $size / 2, $square_xy_2 + $square_xy_1 / 2, $black);
+    imageline($image, $x4, $square_xy_2, $size / 2, $square_xy_2 + $square_xy_1 / 2, $black);
     imageline($image, $square_xy_1 / 2, $size / 2, $square_xy_1, $size / 2, $black);
 
-    imagearc($image, $size / 2, $size / 2, $size - $square_xy_1, $size - $square_xy_1, 260, 280, $black);
-    imagearc($image, $size / 2, $size / 2, $size - $square_xy_1, $size - $square_xy_1, 350, 10, $black);
-    imagearc($image, $size / 2, $size / 2, $size - $square_xy_1, $size - $square_xy_1, 80, 100, $black);
-    imagearc($image, $size / 2, $size / 2, $size - $square_xy_1, $size - $square_xy_1, 170, 190, $black);
+    imagearc($image, $x4, $y4, $size - $square_xy_1, $size - $square_xy_1, 260, 280, $black);
+    imagearc($image, $x4, $y4, $size - $square_xy_1, $size - $square_xy_1, 350, 10, $black);
+    imagearc($image, $x4, $y4, $size - $square_xy_1, $size - $square_xy_1, 80, 100, $black);
+    imagearc($image, $x4, $y4, $size - $square_xy_1, $size - $square_xy_1, 170, 190, $black);
 
     imagefilledellipse($image, $size / 4, $size / 4, $size / 100, $size / 100, $black);
-    imagefilledellipse($image, $size / 2, $size / 4, $size / 100, $size / 100, $black);
+    imagefilledellipse($image, $x4, $size / 4, $size / 100, $size / 100, $black);
     imagefilledellipse($image, $size - $size / 4, $size / 4, $size / 100, $size / 100, $black);
-    imagefilledellipse($image, $size / 4, $size / 2, $size / 100, $size / 100, $black);
-    imagefilledellipse($image, $size / 2, $size / 2, $size / 100, $size / 100, $black);
-    imagefilledellipse($image, $size - $size / 4, $size / 2, $size / 100, $size / 100, $black);
+    imagefilledellipse($image, $size / 4, $y4, $size / 100, $size / 100, $black);
+    imagefilledellipse($image, $x4, $y4, $size / 100, $size / 100, $black);
+    imagefilledellipse($image, $size - $size / 4, $y4, $size / 100, $size / 100, $black);
     imagefilledellipse($image, $size / 4, $size - $size / 4, $size / 100, $size / 100, $black);
-    imagefilledellipse($image, $size / 2, $size - $size / 4, $size / 100, $size / 100, $black);
+    imagefilledellipse($image, $x4, $size - $size / 4, $size / 100, $size / 100, $black);
     imagefilledellipse($image, $size - $size / 4, $size - $size / 4, $size / 100, $size / 100, $black);
 
     imagettftext($image, $font_size, 0, $size / 4 - $size / 100, $size / 4 - $size / 100, $black, $font, '1');
